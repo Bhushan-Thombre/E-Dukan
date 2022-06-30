c63f725f6da7f291edfc96558573f421f97e547d
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-06-30 19:18:12 +0530

    Express setup using ES6 modules

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.listen(5000, console.log('Server started at port 5000'));
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import products from '../products';
 import Rating from '../components/Rating';
+import axios from 'axios';
 
 const ProductScreen = () => {
   const { id } = useParams();
-  const product = products.find((p) => p._id === id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await axios.get(`/api/products/${id}`);
+      setProduct(res.data);
+    };
+    fetchProduct();
+  }, [id]);
+
   return (
     <>
       <Link className="btn btn-dark my-3" to="/">
