dfe92ef642b0f641297cb929c63d5b90f4bc0bd0
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-07-07 16:21:46 +0530

    User Login Screen and Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,35 +1,44 @@
 import axios from 'axios';
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from '../constants/userConstants.js';
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+} from '../constants/userConstants.js';
 
-const login = (email, password) = async (dispatch) => {
-    try {
-        dispatch({
-            type: USER_LOGIN_REQUEST
-        });
+const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
-        const res = await axios.post('/api/users/login', { email, password }, config);
+    const res = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    );
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: res.data
-        });
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: res.data,
+    });
 
-        localStorage.setItem('userInfo', JSON.stringify(res.data));
-    } catch (error) {
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload:
-              error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message,
-          });
-    }
+    localStorage.setItem('userInfo', JSON.stringify(res.data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
 export { login };
