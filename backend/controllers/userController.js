174d040e8b9a327ec263969ed5a0b688cf887d8b
Bhushan Thombre <bhushanthombre2001@gmail.com>
2022-08-04 00:32:39 +0530

    Get user by Id and update user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -125,6 +125,42 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Get user by Id
+// @route GET /api/users/:id
+// @access Private/Admin
+// Admin edit user
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (user) {
+    res.json(user);
+  }
+});
+
+// @desc Update User
+// @route PUT /api/users/:id
+// @access Private/Admin
+// Admin edit user
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+    res.json({
+      id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   registerUser,
@@ -132,4 +168,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
